7707502001df11637fb07c6f04aefa0c5acc2e04
Mehrin Muhammed <mehrinmuhammed2001@gmail.com>
2023-12-27 14:27:47 +0530

    first react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import randomColor from "randomcolor";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App (){
+
+  // lets declare a state
+    const [counter,setCounter] = useState(0)
+    const [color,setColor] = useState('blue')
+    // useEffect
+     useEffect(()=>{
+        console.log("useEffect")
+        setColor(randomColor())
+     }, [counter])
+
+    // declare function
+    function handlerIncrement(){
+    
+      setCounter((prevState)=>{
+        return  prevState + 1
+      })
+      // setColor(randomColor())
+    }
+    function handlerDecrement(){
+    
+      setCounter((prevState)=>{
+        return  prevState - 1
+      })
+      // setColor(randomColor())
+    }
+return(
+<div className="counter">
+  <h1 style={{ color: color, textAlign: 'center' }}>Counter: {counter}</h1> 
+  <button onClick={handlerIncrement}>Increment</button> 
+  <button onClick={handlerDecrement}>Decrement</button> 
+</div>
+
+
+)
 }
 
 export default App;
